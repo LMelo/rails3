a85d2bb8f57aeb1cfc63d49a2845a403b6409c3d
LMelo <luis.nickelback@gmail.com>
2013-12-19 08:18:59 -0200

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Erp::Application.config.secret_token = 'a5c893d49052135ce4bf6a2d7c798bce18748572f8c498d68cf8384e7dec0de3f09c7e0b8714ac02e6001a6f6eed99c9e508875df36fc065fe2b98fb516c39cd'
