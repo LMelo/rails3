134a87760e82a9192c6782d9e8869f53e48f9cbb
LMelo <luis.nickelback@gmail.com>
2013-12-19 09:07:56 -0200

    Scaffold Pessoa (simple form)

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,3 +1,10 @@
 class Pessoa < ActiveRecord::Base
   attr_accessible :ativo, :data_nascimento, :nome
+
+  after_initialize :default_values
+
+  def default_values
+    self.ativo ||= true
+  end
+
 end
